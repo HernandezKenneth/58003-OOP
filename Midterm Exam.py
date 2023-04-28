cbd12c782b4a2a33162ab960d5334133e22ff65d
HernandezKenneth <123909132+HernandezKenneth@users.noreply.github.com>
2023-04-28 08:54:09 +0800

    Add files via upload

---
+++ Midterm Exam.py
@@ -0,0 +1,16 @@
+import math
+class Circle:
+    def __init__(self):
+        self.radius = float(input("Enter the radius of the circle: "))
+        self.diameter = 2 * self.radius
+
+    def area(self):
+        return 3.14 * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
+
+c = Circle()
+print("Area of circle:", c.area())
+print("Perimeter of circle:", c.perimeter())
